5584c4db26a049166e9f179ecc7b3a000a6324f7
RDD2003 <rdurandiez@gmail.com>
2024-02-29 11:10:07 +0100

    curso de typescript

---
+++ TypeScript/HolaMundoTypescript/funciones.js
@@ -0,0 +1,12 @@
+"use strict";
+let saludo = "Hola mundo desde TypeScrip ";
+let numero;
+numero = 10;
+let cualquiera;
+const PI = 3.14;
+function saludar() {
+    console.log(saludo);
+    console.log(numero);
+    console.log(PI);
+}
+saludar();
---
+++ TypeScript/HolaMundoTypescript/funciones.ts
@@ -0,0 +1,16 @@
+let saludo = "Hola mundo desde TypeScrip ";
+
+let numero:number;
+numero = 10;
+
+let cualquiera: any;
+
+const PI = 3.14;
+
+function saludar(){
+    console.log(saludo);
+    console.log(numero);
+    console.log(PI);
+}
+
+saludar();
